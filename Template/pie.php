8be26db01ca8ed6ea884ff06cf86c3c7d52b46eb
lixyleon <123250641+lixyleon@users.noreply.github.com>
2023-07-05 19:22:34 -0400

    Footer Home page Creación del footer de la pagina principal, contenido en el archivo pie.php dentro de la carpeta Template

---
+++ Template/pie.php
@@ -0,0 +1,60 @@
+<footer class="text-between text-lg-start bg-white text-muted ">
+    <section class="text-bg-primary p-2">
+        <div class="container text-center text-md-start mt-4">
+            <div class="row mt-4">
+                <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        <i class="fas fa-gem me-3 text-secondary"></i>FarmaSalud,C.A
+                    </h6>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti, expedita quas pariatur odit quod recusandae, error unde tempore architecto necessitatibus eaque incidunt porro ducimus. Velit doloremque id in reiciendis nesciunt.
+                    </p>
+                </div>
+                <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Productos y Servicios
+                    </h6>
+                    <p>
+                        <a href="productos.php" class="text-reset">Ofertas</a>
+                    </p>
+                    <p>
+                        <a href="productos.php" class="text-reset">Consulta médica en línea</a>
+                    </p>
+                    <p>
+                        <a href="productos.php" class="text-reset">Equipos médicos</a>
+                    </p>
+                    <p>
+                        <a href="productos.php" class="text-reset">Envíos</a>
+                    </p>
+                </div>
+
+                <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mb-md-0 mb-4">
+
+                    <h6 class="text-uppercase fw-bold mb-4">Contacto</h6>
+                    <p><i class="fas fa-home me-3 text-secondary"> Caracas, Venezuela</i></p>
+                    <p>
+                        <i class="fas fa-envelope me-3 text-secondary">
+                            info@example.com
+                        </i>
+                    </p>
+                    <p><i class="fas fa-phone me-3 text-secondary"> + 58 234 567 88</i></p>
+                    <p><i class="fas fa-print me-3 text-secondary"> +58 234 567 89</i></p>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.025);">
+        © 2023 Copyright:
+        <a class="text-reset fw-bold" href="#">Proyecto_Uneweb</a>
+    </div>
+</footer>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
