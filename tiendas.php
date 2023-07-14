8fac62430b823fc480375d458a7e1cd61a88ca57
lixyleon <123250641+lixyleon@users.noreply.github.com>
2023-07-13 20:34:01 -0400

    Creación de la pagina de tiendas Se crea la seccion donde se muestran fotos de las tiendas dirección y teléfonos de contacto

---
+++ tiendas.php
@@ -0,0 +1,35 @@
+<?php include("template/cabecera.php"); ?>
+
+
+<div class="row row-cols-1 row-cols-md-3 g-4 m-4">
+  <div class="col">
+    <div class="card">
+      <img src="imagenes/farmaceutico-comprobando-medicamentos-farmacia.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">La Florida</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="imagenes/farmaceutico-joven-farmacia.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Altamira</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="imagenes/pharmasict-sirviendo-al-cliente-farmacia.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Los Chaguramos</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  </div>
+
+
+<?php include("template/pie_pagina.php"); ?>
