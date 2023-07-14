ddb6004f459299aa229889b492675494db28eeec
lixyleon <123250641+lixyleon@users.noreply.github.com>
2023-07-13 21:07:33 -0400

    Creacion de seccion de ofertas en pagina principal Se crea la seccion de ofertas de productos y se incorporan las imagenes en la carpeta que corresponde

--- index.php
+++ index.php
@@ -1,6 +1,149 @@
 <?php include("template/cabecera.php"); ?>
 
-<br>
+<div class="container-fluid p-4  d-flex ">
+  <div class="card mb-3">
+    <div class="row g-0 ">
+      <div class="col-md-4">
+        <img src="imagenes/farmaceutico-comprobando-medicamentos-farmacia.jpg" class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8 gradient">
+        <div class="card-body">
+          <h1 class="card-title text-center text-primary">Te esperamos en Farmasalud 
+            <br>
+            <br>
+          <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-heart-pulse-fill text-primary" viewBox="0 0 16 16">
+              <path d="M1.475 9C2.702 10.84 4.779 12.871 8 15c3.221-2.129 5.298-4.16 6.525-6H12a.5.5 0 0 1-.464-.314l-1.457-3.642-1.598 5.593a.5.5 0 0 1-.945.049L5.889 6.568l-1.473 2.21A.5.5 0 0 1 4 9H1.475Z" />
+              <path d="M.88 8C-2.427 1.68 4.41-2 7.823 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C11.59-2 18.426 1.68 15.12 8h-2.783l-1.874-4.686a.5.5 0 0 0-.945.049L7.921 8.956 6.464 5.314a.5.5 0 0 0-.88-.091L3.732 8H.88Z" />
+            </svg>
+          </h1>
+          <p class="text-center lead fs-4"> Con la mejor atención, productos y servicios en todas nuestras tiendas</p>
+          <p class="card-text"><small class="text-body-secondary">Imagen de senivpetro en Freepik</small></p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+<section id="ofertas">
+
+  <div class="container-fluid text-center ">
+    <h1 class="text-primary">Ofertas destacadas</h1>
+    <p class="mt-3">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia qui a inventore sit ea sequi dolorum vel eum, ex ut accusantium, itaque cum enim reiciendis numquam cupiditate quaerat expedita porro.
+    </p>
+
+  </div>
+
+  <div class="container-fluid">
+
+    <div class="row row-cols-1 row-cols-md-3 g-4 py-5">
+
+      <div class="col">
+        <div class="card h-100">
+          <div class="card-container">
+            <img src="imagenes/burgesic.png" class="card-img-top " alt="Brugesic">
+          </div>
+          <div class="card-body">
+            <h5 class="card-title text-center fs-3">Brugesic 500Mg</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+              dignissimos accusantium amet similique velit iste.</p>
+          </div>
+          <div class="mb-5 d-flex justify-content-around">
+            <h3 class="text-primary fs-3">30% de Descuento</h3>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <div class="card-container">
+            <img src="imagenes/crema.jpg" class="card-img-top" alt="crema">
+          </div>
+          <div class="card-body">
+            <h5 class="card-title text-center fs-3">Crema Ponds</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+              dignissimos accusantium amet similique velit iste.</p>
+          </div>
+          <div class="mb-5 d-flex justify-content-around gradient">
+            <h3 class="text-primary fs-3">40% de Descuento</h3>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100" >
+          <div class="card-container" >
+            <img src="imagenes/pañal.jpg" class="card-img-top" alt="Pañal">
+          </div>
+          <div class="card-body">
+            <h5 class="card-title text-center fs-3">Pañal</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+              dignissimos accusantium amet similique velit iste.</p>
+          </div>
+          <div class="mb-5 d-flex justify-content-around gradient">
+            <h3 class="text-primary fs-3">25% de Descuento</h3>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <div class="card-container">
+            <img src="imagenes/protector.jpg" class="card-img-top" alt="protector">
+          </div>
+          <div class="card-body">
+            <h5 class="card-title text-center fs-3">Protector</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+              dignissimos accusantium amet similique velit iste.</p>
+          </div>
+          <div class="mb-5 d-flex justify-content-around gradient">
+            <h3 class="text-primary fs-3">25% de Descuento</h3>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100" >
+          <div class="card-container">
+            <img src="imagenes/shampoo.jpg" class="card-img-top" alt="shampoo">
+          </div>
+
+          <div class="card-body">
+            <h5 class="card-title text-center fs-3">Shampoo</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+              dignissimos accusantium amet similique velit iste.</p>
+          </div>
+          <div class="mb-5 d-flex justify-content-around gradient">
+            <h3 class="text-primary fs-3">30% de descuento</h3>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <div class="card-container">
+            <img src="imagenes/equipos-médicos.webp" class="card-img-top" alt="Equipos médicos">
+          </div>
+          <div class="card-body">
+            <h5 class="card-title text-center fs-3">Equipos médicos</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+              dignissimos accusantium amet similique velit iste.</p>
+          </div>
+          <div class="mb-5 d-flex justify-content-around gradient">
+            <h3 class="text-primary fs-3">10$ de descuento</h3>
+
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+
+</section>
 <br>
 
 <?php include("template/pie_pagina.php"); ?>
