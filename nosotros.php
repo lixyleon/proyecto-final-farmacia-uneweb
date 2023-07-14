f3b971512ccf726a08215fb9a3e333f895bb3cde
lixyleon <123250641+lixyleon@users.noreply.github.com>
2023-07-13 22:20:56 -0400

    Creacion de la pagina nosotros Se crea la pagina denominada nosotros donde presenta al equipo que conforma la empresa

---
+++ nosotros.php
@@ -0,0 +1,130 @@
+<?php include("template/cabecera.php"); ?>
+
+
+<div class="container-fluid py-3 text-center">
+  <h1 class="display-5 fw-bold text-primary">Nosotros</h1>
+  <p class="fs-4">Somos un equipo dedicado al servicio de la salud. 
+    Iniciamos en el año 2021 y hemos crecido rápidamente, por dar un servicio de alta calidad</p>
+  
+</div>
+
+<h2 class="m-4">Nuestro equipo</h2>
+<div class="container-fluid container-slider">
+  <input type="radio" name="dot" id="one">
+  <input type="radio" name="dot" id="two">
+  <div class="button button-slider">
+    <label for="one" class=" active one"></label>
+    <label for="two" class="two"></label>
+  </div>
+  <div class="main-card main-card-slider">
+    <div class="cards cards-slider ">
+      <div class="card card-slider bg-primary">
+        <div class="content content-slider">
+          <div class="img img-slider">
+            <img src="imagenes/Dra.webp" alt="">
+          </div>
+          <div class="details">
+            <div class="name text-white">Andrew Neil</div>
+            <div class="job text-white">Web Designer</div>
+          </div>
+          <div class="media-icons">
+            <a id="icons-slider" href="#"><i class="bi bi-facebook"></i></a>
+            <a id="icons-slider" href="#"><i class="bi bi-twitter"></i></a>
+            <a id="icons-slider" href="#"><i class="bi bi-instagram"></i></a>
+            <a id="icons-slider" href="#"><i class="bi bi-youtube"></i></a>
+          </div>
+        </div>
+      </div>
+      <div class="card card-slider">
+        <div class="content content-slider">
+          <div class="img img-slider">
+            <img src="imagenes/feliz-joven-repartidora-uniforme-azul-gorra-caja-carton-portapapeles-mirando-sonrisa-cara-pie-sobre-pared-naranja.jpg" alt="">
+          </div>
+          <div class="details">
+            <div class="name">Jasmine Carter</div>
+            <div class="job">UI Designer</div>
+          </div>
+          <div class="media-icons">
+            <a id="icons-slider" href="#"><i class="bi bi-facebook"></i></a>
+            <a id="icons-slider" href="#"><i class="bi bi-twitter"></i></a>
+            <a id="icons-slider" href="#"><i class="bi bi-instagram"></i></a>
+            <a id="icons-slider" href="#"><i class="bi bi-youtube"></i></a>
+          </div>
+        </div>
+      </div>
+      <div class="card card-slider">
+        <div class="content content-slider">
+          <div class="img img-slider">
+            <img src="imagenes/elegante-empresaria-segura-sonriendo.jpg" alt="">
+          </div>
+          <div class="details">
+            <div class="name">Justin Chung</div>
+            <div class="job">Web Devloper</div>
+          </div>
+          <div class="media-icons">
+            <a id="icons-slider" href="#"><i class="bi bi-facebook"></i></a>
+            <a id="icons-slider" href="#"><i class="bi bi-twitter"></i></a>
+            <a id="icons-slider" href="#"><i class="bi bi-instagram"></i></a>
+            <a id="icons-slider" href="#"><i class="bi bi-youtube"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="cards cards-slider">
+      <div class="card card-slider">
+        <div class="content content-slider">
+          <div class="img img-slider">
+            <img src="imagenes/esperanza-optimista-linda-mujer.jpg" alt="">
+          </div>
+          <div class="details">
+            <div class="name">Appolo Reef</div>
+            <div class="job">Web Designer</div>
+          </div>
+          <div class="media-icons">
+            <a id="icons-slider" href="#"><i class="bi bi-facebook"></i></a>
+            <a id="icons-slider" href="#"><i class="bi bi-twitter"></i></a>
+            <a id="icons-slider" href="#"><i class="bi bi-instagram"></i></a>
+            <a id="icons-slider" href="#"><i class="bi bi-youtube"></i></a>
+          </div>
+        </div>
+      </div>
+      <div class="card card-slider">
+        <div class="content content-slider">
+          <div class="img img-slider">
+            <img src="imagenes/image_banner.jpeg" alt="">
+          </div>
+          <div class="details">
+            <div class="name">Adrina Calvo</div>
+            <div class="job">UI Designer</div>
+          </div>
+          <div class="media-icons">
+            <a id="icons-slider" href="#"><i class="bi bi-facebook"></i></a>
+            <a id="icons-slider" href="#"><i class="bi bi-twitter"></i></a>
+            <a id="icons-slider" href="#"><i class="bi bi-instagram"></i></a>
+            <a id="icons-slider" href="#"><i class="bi bi-youtube"></i></a>
+          </div>
+        </div>
+      </div>
+      <div class="card card-slider bg-primary">
+        <div class="content content-slider ">
+          <div class="img img-slider">
+            <img src="imagenes/pharmasict-sirviendo-al-cliente-farmacia.jpg" alt="">
+          </div>
+          <div class="details">
+            <div class="name text-white">Nicole Lewis</div>
+            <div class="job text-white">Web Devloper</div>
+          </div>
+          <div class="media-icons">
+            <a id="icons-slider" href="#"><i class="bi bi-facebook"></i></a>
+            <a id="icons-slider" href="#"><i class="bi bi-twitter"></i></a>
+            <a id="icons-slider" href="#"><i class="bi bi-instagram"></i></a>
+            <a id="icons-slider" href="#"><i class="bi bi-youtube"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+<?php include("template/pie_pagina.php"); ?>
